2974f66c6476bd0cc888e9a74726405ec345e27f
sajal singh <singhsajal920@gmail.com>
2025-11-13 03:46:09 +0530

    add iam role to access cross account s3

--- envs/shared/main.tf
+++ envs/shared/main.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
-#   backend "s3" {
-#     bucket         = "my-terraform-prod-state"
-#     key            = "staging/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-lock"
-#     encrypt        = true
-#   }
+  #   backend "s3" {
+  #     bucket         = "my-terraform-prod-state"
+  #     key            = "staging/terraform.tfstate"
+  #     region         = "us-east-1"
+  #     dynamodb_table = "terraform-lock"
+  #     encrypt        = true
+  #   }
 
   required_providers {
     aws = {
@@ -17,10 +17,20 @@ terraform {
   }
 }
 
+# provider "aws" {
+#   region = var.aws_region
+#   profile = "shared-account"
+# }
+
 provider "aws" {
-  region = var.aws_region
-  profile = "shared-account"
+  region = "ap-south-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::583534901542:role/terraform-backend-access-role"
+    session_name = "jenkins-terraform"
+  }
 }
+
 
 module "vpc" {
   source = "../../modules/vpc"
