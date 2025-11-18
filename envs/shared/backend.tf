1c654e26019d15b5f996cad6473cf6a36859e892
sajal singh <singhsajal920@gmail.com>
2025-11-18 12:38:13 +0530

    modify shared backend region

--- envs/shared/backend.tf
+++ envs/shared/backend.tf
@@ -1,3 +1,14 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "doctorlink-tf-state"
+#     key            = "shared/terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform-locks"
+#     encrypt        = true
+#     profile        = "shared-account"
+#   }
+# }
+
 terraform {
   backend "s3" {
     bucket         = "doctorlink-tf-state"
@@ -5,6 +16,5 @@ terraform {
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
-    profile        = "shared-account"
   }
 }
